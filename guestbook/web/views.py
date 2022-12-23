cad331e61cfb8462fcaddcf0209aefc154f73b2f
Honson Chi <honsonchi@gmail.com>
2022-12-23 10:51:56 +0800

    Msg List,View,Create

--- guestbook/web/views.py
+++ guestbook/web/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DeleteView,CreateView
+from .models import Message
+from django.urls import reverse_lazy
 # Create your views here.
+
+class MessageList(ListView):
+    model = Message
+    ordering = ['-id']
+
+class MessageDetail(DeleteView):
+    model = Message
+
+class MessageCreate(CreateView):
+    model = Message
+    fields = ['user','subject','content']
+    success_url = reverse_lazy('msg_list')
